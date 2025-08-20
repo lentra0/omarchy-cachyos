da8bcac9b4df16df9149a670f6029124a46af831
lentra0 <151880384+lentra0@users.noreply.github.com>
2025-08-20 17:09:34 +0000

    added bluetui

--- install/desktop/bluetooth.sh
+++ install/desktop/bluetooth.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install bluetooth controls
-yay -S --noconfirm --needed blueberry
+yay -S --noconfirm --needed bluetui
 
 # Turn on bluetooth by default
 sudo systemctl enable --now bluetooth.service
