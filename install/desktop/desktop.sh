322a61f49acc3b50f4474b9b5a18c08eb83cf8b4
lentra0 <151880384+lentra0@users.noreply.github.com>
2025-08-20 17:07:57 +0000

    added fcitx configtool and removed chromium

--- install/desktop/desktop.sh
+++ install/desktop/desktop.sh
@@ -2,11 +2,10 @@
 
 yay -S --noconfirm --needed \
   brightnessctl playerctl pamixer wiremix wireplumber \
-  fcitx5 fcitx5-gtk fcitx5-qt wl-clip-persist \
+  fcitx5 fcitx5-gtk fcitx5-qt fcitx-configtool wl-clip-persist \
   nautilus sushi ffmpegthumbnailer gvfs-mtp \
   slurp satty \
-  mpv evince imv \
-  chromium
+  mpv evince imv
 
 # Add screen recorder based on GPU
 if lspci | grep -qi 'nvidia'; then
