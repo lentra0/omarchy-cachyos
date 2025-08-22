7a966632622127ef354a0e199d8d6e43960db6c2
lentra0 <151880384+lentra0@users.noreply.github.com>
2025-08-22 16:41:55 +0300

    added qt5-wayland package

--- install/desktop/hyprlandia.sh
+++ install/desktop/hyprlandia.sh
@@ -3,4 +3,5 @@
 yay -S --noconfirm --needed \
   hyprland hyprshot hyprpicker hyprlock hypridle hyprsunset polkit-gnome hyprland-qtutils \
   walker-bin libqalculate waybar mako swaybg swayosd \
-  xdg-desktop-portal-hyprland xdg-desktop-portal-gtk
+  xdg-desktop-portal-hyprland xdg-desktop-portal-gtk \
+  qt5-wayland
