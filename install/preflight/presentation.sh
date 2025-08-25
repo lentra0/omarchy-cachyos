1199a003b9b40d0df2169f8535eedca4384555b9
lentra0 <151880384+lentra0@users.noreply.github.com>
2025-08-25 19:19:34 +0300

    Update presentation.sh

--- install/preflight/presentation.sh
+++ install/preflight/presentation.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-yay -S --noconfirm --needed gum python-terminaltexteffects
+if yay -Q python-terminaltexteffects >/dev/null 2>&1; then
+    yay -S --noconfirm --needed gum python-terminaltexteffects
+fi
