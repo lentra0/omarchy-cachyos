767548d80cab6b1641016e3bfa6bdb90fdab4da2
lentra0 <151880384+lentra0@users.noreply.github.com>
2025-08-25 22:19:22 +0300

    added 2.0.0 Omarchy logo to waybar

--- install/desktop/fonts.sh
+++ install/desktop/fonts.sh
@@ -5,3 +5,8 @@ yay -S --noconfirm --needed ttf-font-awesome ttf-cascadia-mono-nerd ttf-ia-write
 if [ -z "$OMARCHY_BARE" ]; then
   yay -S --noconfirm --needed ttf-jetbrains-mono noto-fonts-cjk noto-fonts-extra
 fi
+
+# Omarchy logo in a font for Waybar use
+mkdir -p ~/.local/share/fonts
+cp ~/.local/share/omarchy/default/omarchy.ttf ~/.local/share/fonts/
+fc-cache
