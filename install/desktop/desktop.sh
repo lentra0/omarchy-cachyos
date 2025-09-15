c66a7a8cbd4233f969dc96e78529b761e392cc01
lentra0 <151880384+lentra0@users.noreply.github.com>
2025-09-15 13:09:27 +0300

    added vulkan deps

--- install/desktop/desktop.sh
+++ install/desktop/desktop.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Ensure Vulkan packages are present
+paru -S --noconfirm --needed vulkan-headers vulkan-icd-loader vulkan-validation-layers
+
 paru -S --noconfirm --needed \
   brightnessctl playerctl pamixer wiremix wireplumber \
   fcitx5 fcitx5-gtk fcitx5-qt fcitx5-configtool wl-clip-persist \
